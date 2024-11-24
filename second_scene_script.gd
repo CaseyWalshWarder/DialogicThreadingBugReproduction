e1def60afff9dcb8f979db1dd9cc16f3e66509fd
Casey Walsh-Warder <cwalshwarder@brandeis.edu>
2024-11-24 18:53:32 -0500

    workaround

--- second_scene_script.gd
+++ second_scene_script.gd
@@ -1,8 +1,14 @@
 extends Node
 
+var started_dialogue := false
+
 func _ready():
     Dialogic.signal_event.connect(_on_dialogic_signal)
-    Dialogic.start("timeline_2");
+
+func _process(delta):
+    if !started_dialogue:
+        started_dialogue = true
+        Dialogic.start("timeline_2")
 
 func _on_dialogic_signal(argument:String):
     pass
